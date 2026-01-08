4fdd561f617c53e5000ae32c359acaefe0a3c816
Robert Poat <moonlight_mining@yahoo.com>
2026-01-08 10:37:27 -0800

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,6 +5,7 @@ from app.models.user import User
 from app.models.health_profile import HealthProfile
 from app.models.conversation import Conversation
 from app.models.message import Message
+from app.models.client_message import ClientMessage
 from app.models.symptom import Symptom
 from app.models.treatment import Treatment
 
